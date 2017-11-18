c52fe96e91ade112d02de93e77d2906a5ba84c3a
lhakit <32984792+lhakit@users.noreply.github.com>
2017-11-18 12:26:55 -0600

    finish final project

---
+++ consitution.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 4, 2017
+
+@author: ITAUser
+'''
+#open the file
+#read the file
+#split the file
+#print the number of words in the list
+f = open('constituton.txt' , 'r')
+text = f.read()
+words = text.split()
+len (words)
+print( len (words))
+
+
+
